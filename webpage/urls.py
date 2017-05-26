13a4fd42769d3dfeab4f4fb9195a02301a7d564b
Jacodian <jaco_1031@hotmail.com>
2017-05-26 17:02:03 -0300

    est do     (testpath/"test.cpp").write <<-EOS.undent       #include <opencv/cv.h>       #include <iostream>       int main()       {         std::cout << CV_VERSION << std::endl;         return 0;       }     EOS     system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-o", "test"     assert_equal `./test`.strip, version.to_s

--- webpage/urls.py
+++ webpage/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login$', views.login, name='login'),
+    url(r'^login$', views.login_user, name='login'),
+    url(r'^login/logtry$', views.log_intent, name='logtry'),
     url(r'^signup$', views.signup, name='signup'),
     url(r'^vendedor$', views.perfil_vendedor, name='perfil_vendedor'),
     url(r'^producto$', views.gestion_producto, name='gestion_producto'),
--- webpage/views.py
+++ webpage/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
 
 
 def index(request):
     return render(request, 'webpage/index.html')
 
 
-def login(request):
+def login_user(request):
     return render(request, 'webpage/login.html')
+         
 
 
 def signup(request):
@@ -19,3 +23,23 @@ def perfil_vendedor(request):
 
 def gestion_producto(request):
     return render(request, 'webpage/gestion-productos.html')
+
+
+def log_intent(request):
+	name = request.POST['nombre']
+	passw = request.POST['password']
+	user = authenticate( username=name, password=passw)
+	if user is not None:
+		login(request, user)
+		return render(request, 'webpage/index.html')
+        
+	else:
+		return render(request, 'webpage/login.html', {
+		'error' : 'No ha ingresado un usuario y contraseña válidos'
+		})
+
+
+			
+
+
+
