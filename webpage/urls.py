ced5ccff9bf5d9fe20c8a638c356b7e5f6bdc4c7
Jacodian <jaco_1031@hotmail.com>
2017-05-27 00:33:52 -0300

    agregado manejo de views y urls de perfil de vendedor

--- webpage/urls.py
+++ webpage/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^login/logtry$', views.login_intent, name='logtry'),
     url(r'^signup$', views.signup, name='signup'),
     url(r'^signup/regtry$', views.reg_intent, name='regtry'),
-    url(r'^vendedor$', views.perfil_vendedor, name='perfil_vendedor'),
+    url(r'^vendedor/(?P<nombre_vendedor>\w+)$', views.perfil_vendedor, name='perfil_vendedor'),
     url(r'^producto$', views.gestion_producto, name='gestion_producto'),
+
 ]
