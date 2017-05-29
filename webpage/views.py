a3e7105b331e421360b7d7ca62750f0509b4115f
Andy <andybaloian@hotmail.cl>
2017-05-29 12:38:46 -0300

    requisitos funcionales 12 y 25 (mostrar resumen de productos y poder gestionar productos)

--- webpage/views.py
+++ webpage/views.py
@@ -3,6 +3,7 @@ from datetime import time, datetime
 
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
+from webpage.models import Producto
 from django.contrib.auth import authenticate, login, logout
 from django.http import Http404
 
@@ -25,6 +26,8 @@ def signup(request):
 def perfil_vendedor(request, nombre_vendedor):
     context = dict()
     user = get_object_or_404(User, username=nombre_vendedor)
+    productos = Producto.objects.filter(vendedor=user.vendedor)
+    context['productos'] = productos
     if hasattr(user, 'vendedor'):
         medios_pago = []
         if user.vendedor.acepta_Efectivo:
@@ -44,18 +47,21 @@ def perfil_vendedor(request, nombre_vendedor):
 
 
         if hasattr(user.vendedor, 'vendedorfijo'):
-            context['vendedor'] = user.vendedor.vendedorfijo
+            context['vendedor'] = user.vendedor
             context['fijo'] = True
-            horario_inicio = time(context['vendedor'].horaInicio, context['vendedor'].minutoInicio)
-            horario_fin = time(context['vendedor'].horaFin, context['vendedor'].minutoFin)
+            horario_inicio = time(context['vendedor'].vendedorfijo.horaInicio, context['vendedor'].vendedorfijo.minutoInicio)
+            horario_fin = time(context['vendedor'].vendedorfijo.horaFin, context['vendedor'].vendedorfijo.minutoFin)
             hora_actual = time(datetime.now().hour, datetime.now().minute)
-            context['activo'] = hora_actual >= horario_inicio and hora_actual <= horario_fin
+            if horario_inicio<= horario_fin:
+                context['activo'] =  hora_actual >= horario_inicio and hora_actual <= horario_fin
+            else:
+                context['activo'] =  hora_actual >= horario_inicio or hora_actual <= horario_fin
             context['horario_inicio'] = horario_inicio.strftime("%H:%M")
             context['horario_fin'] = horario_fin.strftime("%H:%M")
         else:
-            context['vendedor'] = user.vendedor.vendedorambulante
+            context['vendedor'] = user.vendedor
             context['fijo'] = False
-            context['activo'] = context['vendedor'].activo
+            context['activo'] = context['vendedor'].vendedorambulante.activo
     else:
         raise Http404("No hay vendedores que tengan el nombre buscado")
     return render(request, 'webpage/vendedor-profile-page.html',context)
@@ -64,8 +70,71 @@ def perfil_vendedor(request, nombre_vendedor):
 
 
 def gestion_producto(request):
-    return render(request, 'webpage/gestion-productos.html')
+    context = dict()
+    if request.user.is_authenticated() and hasattr(request.user, 'vendedor'):
+        context['vendedor'] = request.user
+        return render(request, 'webpage/gestion-productos.html', context)
+    else:
+        raise Http404("No se ha logueado un vendedor")
 
+def agregar_producto(request):
+    if request.user.is_authenticated() and hasattr(request.user, 'vendedor'):
+        context = dict()
+        nombre = request.POST['item']
+        precio = request.POST['precio']
+        stock = request.POST['stock']
+        descripcion = request.POST['descripcion']
+        idFotoPrevia = request.POST['test']
+        foto = request.FILES.get('foto', None)
+        vendedor = Vendedor.objects.get(user=request.user)
+        context['vendedor'] = vendedor
+
+        if len(nombre)>0 and precio.isdigit() and stock.isdigit() and foto!='':
+
+            producto=Producto(vendedor=vendedor, nombre=nombre, foto=foto, fotoPrev=idFotoPrevia, descripcion=descripcion,
+                          stock=stock, precio=precio )
+            producto.save()
+            return redirect(perfil_vendedor, vendedor.user.username)
+        else:
+            return render(request, 'webpage/gestion-productos.html', {'error': "error en el formulario"})
+    else:
+        raise Http404("No se ha logueado un vendedor")
+
+def editar_producto(request, pk_producto):
+    context = dict()
+    if request.user.is_authenticated() and hasattr(request.user, 'vendedor'):
+        context['vendedor'] = request.user
+        context['producto'] = Producto.objects.get(pk=pk_producto)
+        return render(request, 'webpage/gestion-productos.html', context)
+    else:
+        raise Http404("No se ha logueado un vendedor")
+
+def actualizar_producto(request, pk_producto):
+    producto = Producto.objects.get(pk=pk_producto)
+    vendedor = producto.vendedor
+    if request.user.is_authenticated() and hasattr(request.user, 'vendedor'):
+        nuevoNombre = request.POST['item']
+        nuevoPrecio = request.POST['precio']
+        nuevoStock = request.POST['stock']
+        nuevaDescripcion = request.POST['descripcion']
+        nuevoProducto = Producto(nombre=nuevoNombre, precio=nuevoPrecio, stock=nuevoStock, descripcion=nuevaDescripcion,
+                                 vendedor=producto.vendedor, foto=producto.foto, fotoPrev=producto.fotoPrev)
+        nuevoProducto.save()
+        producto.delete()
+        return redirect(perfil_vendedor, vendedor.user.username)
+    else:
+        raise Http404("No se ha logueado un vendedor")
+
+def eliminar_producto(request, pk_producto):
+    context = dict()
+    if request.user.is_authenticated() and hasattr(request.user, 'vendedor'):
+        producto = Producto.objects.get(pk=pk_producto)
+        vendedor=producto.vendedor
+        producto.delete()
+        context['vendedor'] = request.user
+        return redirect(perfil_vendedor, vendedor.user.username)
+    else:
+        raise Http404("No se ha logueado un vendedor")
 
 def logout_intent(request):
     logout(request)
